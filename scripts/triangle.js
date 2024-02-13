60cf76dc4b87789b5e7a773a8cd64b36a481bc59
Hossain3227 <hasan32.hk27@gmail.com>
2024-02-14 02:32:05 +0600

    pentagon added

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -25,6 +25,79 @@ function calculateTriangleArea(){
 //rectangle area 
 
 function calculateRectangleArea(){
- console.log('something');    
+  //get length 
+   const rlInput = document.getElementById('rectangle-length');
+   const rlText = rlInput.value;
+   const length = parseFloat(rlText); 
+   console.log(length);
+
+   //get width 
+
+   const rwInput = document.getElementById('rectangle-width');
+   const rwText = rwInput.value;
+   const width =  parseFloat(rwText)
+   console.log(width);
+
+   //calculate r area 
+
+   const rarea = length * width;
+   console.log(rarea);
+
+   //show rectangle area 
+
+   const raSpan = document.getElementById('rectangle-area');
+   raSpan.innerText = rarea;
+
 }
 
+
+//parallelogram area calculate
+
+function calculatepArea(){
+  //  const plInput = document.getElementById('p-base');
+  //  const plText = plInput.value;
+  //  const pbase = parseFloat(plText); 
+   const pbase = getInputValueId('p-base');
+   console.log(pbase);
+   const pheight = getInputValueId('p-height');
+   console.log(pheight);
+
+   const area = pbase * pheight;
+   console.log(area);
+
+   setinnerTextById('p-area',area);
+   // console.log(pbase);
+}
+
+// function pBase(){
+
+//    const plInput = document.getElementById('p-base');
+//    const plText = plInput.value;
+//    const pbase = parseFloat(plText);
+//    return pbase;
+// }
+
+function getInputValueId(inputfieldid){
+   const inputfield = document.getElementById(inputfieldid);
+   const inputValueText = inputfield.value;
+   const inputValue = parseFloat(inputValueText);
+   // console.log(inputValue);
+   return inputValue;
+}
+
+function setinnerTextById(elementId, area){
+   const element = document.getElementById(elementId);
+   element.innerText = area;
+}
+
+
+function calculatePentaArea(){
+  const pentabase = getInputValueId('penta-length');
+  console.log(pentabase);
+  const pentaheight = getInputValueId('penta-width');
+  console.log(pentaheight); 
+  
+  const pentaArea = 0.5 * pentabase  * pentaheight;
+  console.log(pentaArea);
+  setinnerTextById('penta-area',pentaArea); 
+}
